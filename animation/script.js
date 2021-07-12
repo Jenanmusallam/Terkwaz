064e6a5f08db8287650198aab3dde27e018f275b
JenanMusallam <jenan.musalam@gmail.com>
2021-07-12 11:25:06 +0300

    animation using in my website (musallam photography)

--- animation/script.js
+++ animation/script.js
@@ -5,9 +5,6 @@ AOS.init({
     duration: 1000
 });
 
-// Open & close Header
-
-
 // open & close Header
 const pageHeader = document.querySelector(".header");
 
@@ -36,7 +33,6 @@ toggleMobileMenu.addEventListener("click", () => {
             closeMenu.classList.remove(dNone);
             closeMenu.classList.add(dBlock);
         } else {
-
             openMenu.classList.remove(dNone);
             openMenu.classList.add(dBlock);
             closeMenu.classList.remove(dBlock);
@@ -44,88 +40,3 @@ toggleMobileMenu.addEventListener("click", () => {
         }
     }
 });
-
-// ---------------------------------------//
-
-
-
-const getDateTime = () => {
-    let now = new Date();
-    let year = now.getFullYear();
-    let month = now.getMonth() + 1;
-    let day = now.getDate();
-    let hour = now.getHours();
-    let minute = now.getMinutes();
-    let second = now.getSeconds();
-    if (month.toString().length == 1) {
-        month = '0' + month;
-    }
-    if (day.toString().length == 1) {
-        day = '0' + day;
-    }
-    if (hour.toString().length == 1) {
-        hour = '0' + hour;
-    }
-    if (minute.toString().length == 1) {
-        minute = '0' + minute;
-    }
-    if (second.toString().length == 1) {
-        second = '0' + second;
-    }
-    let dateTime = year + '/' + month + '/' + day + ' ' + hour + ':' + minute + ':' + second;
-    return dateTime;
-}
-
-// example usage: realtime clock
-setInterval(function () {
-    currentTime = getDateTime();
-    document.querySelector(".time").textContent = currentTime;
-}, 1000);
-
-// Scroll
-const intro = document.querySelector(".intro");
-const video = intro.querySelector("video");
-const text = intro.querySelector("h1");
-//END SECTION
-const section = document.querySelector("section");
-const end = section.querySelector("h1");
-
-//SCROLLMAGIC
-const controller = new ScrollMagic.Controller();
-
-//Scenes
-let scene = new ScrollMagic.Scene({
-    duration: 9000,
-    triggerElement: intro,
-    triggerHook: 0
-})
-    .addIndicators()
-    .setPin(intro)
-    .addTo(controller);
-
-//Text Animation
-const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
-
-let scene2 = new ScrollMagic.Scene({
-    duration: 3000,
-    triggerElement: intro,
-    triggerHook: 0
-})
-    .setTween(textAnim)
-    .addTo(controller);
-
-//Video Animation
-let accelamount = 0.1;
-let scrollpos = 0;
-let delay = 0;
-
-scene.on("update", e => {
-    scrollpos = e.scrollPos / 1000;
-});
-
-setInterval(() => {
-    delay += (scrollpos - delay) * accelamount;
-    console.log(scrollpos, delay);
-
-    video.currentTime = delay;
-}, 33.3);
